adc4aa28d2129f6944044e20365169c3931cfba4
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-08 15:02:00 -0800

    add comment

--- src/TestJunit.java
+++ src/TestJunit.java
@@ -7,7 +7,9 @@ public class TestJunit {
 	   String message = "Hello World";	
 	   EchoChamber echoChamber = new EchoChamber(message);
 
+
 	   @Test
+     // The test should fail here
 	   public void testPrintMessage() {
 		  message = "New World"; 
 	      assertEquals(message, echoChamber.sendBackSound());
