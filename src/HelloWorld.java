79865cf832a2b3a6c26bacf9d4331f47a37632a0
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-10 10:29:44 -0800

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
-// print hello world3
 public class HelloWorld {
-
+	
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
