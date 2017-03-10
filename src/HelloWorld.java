5e6912a8f0c7bc25243bdbb85363879399feef54
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-10 15:22:09 -0800

    add successful and failed builds

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
 public class HelloWorld {
-	
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
+	 
+    // // Demo code for successful build
+    // public static void main(String[] args) {
+    //     System.out.println("Hello, World");
+    // }
 
+    // Demo code for failed build
+    public static void main(String[] args) {
+        System.println("Hello, World");
+    }
 }
