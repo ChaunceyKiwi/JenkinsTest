615bf09f47802e5bf869312db7f7b3f9a1692785
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-10 15:25:44 -0800

    trigger

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
 	 
-    // Demo code for successful build
+    // The demo code for successful build
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
 
-    // // Demo code for failed build
+    // // The demo code for failed build
     // public static void main(String[] args) {
     //     System.println("Hello, World");
     // }
