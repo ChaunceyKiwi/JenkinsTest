37757e76b137d72952daeceff68e82d8d8742a58
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-10 15:29:42 -0800

    updated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,7 @@
 public class HelloWorld {
-	 
-    // The demo code for successful build
+	
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
 
-    // // The demo code for failed build
-    // public static void main(String[] args) {
-    //     System.println("Hello, World");
-    // }
 }
