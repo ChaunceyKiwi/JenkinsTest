096ac39d2a9a1244af73f637c186e67a06822c2b
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-03-10 16:40:30 -0800

    try error demo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
-	
+	 
+    // // Successful demo
+    // public static void main(String[] args) {
+    //     System.out.println("Hello, World");
+    // }
+
+    // Error demo
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.println("Hello, World");
     }
 
 }
